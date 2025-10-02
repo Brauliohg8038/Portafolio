fa8a4b152f6aac6d31de321050bcd58dd2cde921
Braulio H <braulioherre98@gmail.com>
2025-10-02 04:08:26 -0600

    Update - all

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./**/*.html",
+    "./src/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
